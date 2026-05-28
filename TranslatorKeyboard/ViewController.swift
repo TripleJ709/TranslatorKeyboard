844682aca70032ab032b1ad67abc89d9b0fad2dd
TripleJ709 <ijn9907@gmail.com>
2026-05-28 21:37:27 +0900

    initial Xcode project setup

---
+++ TranslatorKeyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslatorKeyboard
+//
+//  Created by 장주진 on 5/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
